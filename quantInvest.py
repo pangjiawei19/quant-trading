e1644f18e6132d3f1fa5e2e295ab231e67dbd33a
pangjiawei <pangjiawei19@gmail.com>
2024-01-25 10:36:44 +0800

    feat: add script of generate trading date

---
+++ quantInvest.py
@@ -0,0 +1,4 @@
+import datetime
+import numpy as np
+import pandas as pd
+
---
+++ script/generate_trading_date.py
@@ -0,0 +1,6 @@
+import akshare as ak
+
+df = ak.tool_trade_date_hist_sina()
+
+print(df)
+df.to_csv('../csv/trading_date.csv', index=True, header=True)
